65c07349776a3443f388525190aa76bb173a1bbb
admin <admin@MacBook-Pro-de-admin.local>
2022-04-28 08:53:40 -0400

    validacion formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,20 +1,50 @@
-
+import { useState } from 'react'
+import useClima from '../hooks/useClima'
 
 const Formulario = () => {
+
+    const [alerta, setAlerta] = useState('')
+
+    const { busqueda, datosBusqueda, consultarClima } = useClima()
+    const { ciudad, pais } = busqueda
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(Object.values(busqueda).includes('')){
+            setAlerta('Todos los campos son Obligatorios')
+            return
+        }
+
+        consultarClima(busqueda)
+    }
+
   return (
     <div className="contenedor">
-        <form>
+
+        { alerta && <p>{alerta}</p> }
+        <form
+            onSubmit={handleSubmit}
+        >
             <div className="campo">
                 <label htmlFor="ciudad">Ciudad</label>
                 <input 
                     type="text"
                     id="ciudad"
                     name="ciudad"
+                    onChange = {datosBusqueda}
+                    value={ciudad}
                 />
             </div>
             <div className="campo">
                 <label htmlFor="pais">Pais</label>
-                <select id="pais" name="pais">
+                <select 
+                    id="pais" 
+                    name="pais"
+                    onChange={datosBusqueda}
+                    value={pais}
+                
+                >
                     <option value="">Seleccione un Pais</option>
                     <option value="CL">Chile</option>
                     <option value="CO">Colombia</option>
--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -16,12 +16,17 @@ const ClimaProvider = ({children}) => {
         })
     }
 
+    const consultarClima = datos => {
+        console.log(datos)
+    }
+
     return(
 
         <ClimaContext.Provider 
            value={{
                 busqueda,
-                datosBusqueda
+                datosBusqueda,
+                consultarClima
            }} 
         >
             {children}
